9bc7a30043e060243f87b5b869da87d45ca63b36
Eli Entelis <elientelis1999@gmail.com>
2020-03-30 13:00:21 +0300

    added library fontawasome

--- server.js
+++ server.js
@@ -7,15 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname });
 });
 
-const testFolder = './src/';
-
-
-fs.readdir(testFolder, (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-    console.log('hey')
-  });
-});
 
 app.listen(3000, () => console.log("app listening on port 3000"))
 
--- src/functions.js
+++ src/functions.js
@@ -125,3 +125,4 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
