f6b4cb4d32a03384474fae3014962e5587897ffe
Eli Entelis <elientelis1999@gmail.com>
2020-03-30 10:24:35 +0300

    added pdf and dropdown at sidenav

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express")
 const app = express()
+const fs = require("fs")
 
 app.use(express.static(__dirname))
 app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname });
 });
+
+const testFolder = './src/';
+
+
+fs.readdir(testFolder, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+    console.log('hey')
+  });
+});
+
 app.listen(3000, () => console.log("app listening on port 3000"))
+
