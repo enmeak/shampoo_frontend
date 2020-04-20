e895eed0a25cf843d0eaa64706fefd2d928238a7
Eli Entelis <elientelis1999@gmail.com>
2020-04-21 01:29:38 +0300

    added ajax pdf loading

--- src/functions.js
+++ src/functions.js
@@ -2,7 +2,7 @@
 const tab_ids = ["home", "material", "completed_exams", "scheduale"];
 
 // validate password match at signup
-$("#confirm_sign_up_password").on("focusout", function() {
+$("#confirm_sign_up_password").on("focusout", function () {
   if ($("#sign_up_password").val() == $("#confirm_sign_up_password").val()) {
     $("#error_signup").hide();
   } else $("#error_signup").show();
@@ -42,7 +42,7 @@ function signIn() {
 
   const url = `http://localhost:5000/users/signIn/${user_name}/${password}`;
 
-  $.get(url, function(data, status) {
+  $.get(url, function (data, status) {
     if (data == true) {
       showMain();
       showNav();
@@ -131,7 +131,7 @@ function switchTab(tab_id) {
 
 // function to highlight chosen tab
 function activateTabNav(tab_id) {
-  tab_ids.forEach(id => {
+  tab_ids.forEach((id) => {
     if (id == tab_id) {
       $("#" + id).addClass("active");
     } else {
@@ -142,7 +142,7 @@ function activateTabNav(tab_id) {
 
 // function to show chosen content according to chosen tab
 function showContent(content_id) {
-  tab_ids.forEach(id => {
+  tab_ids.forEach((id) => {
     if (id == content_id) {
       $("#" + id + "_content").show();
     } else {
@@ -166,7 +166,7 @@ var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
@@ -177,17 +177,18 @@ for (i = 0; i < dropdown.length; i++) {
   });
 }
 
-
 // load documents
-$(document).ready(function(){
-  $("#linux").click(function(){
-      $("#pdf").load("/src/material.html", function(responseTxt, statusTxt, jqXHR){
-          if(statusTxt == "success"){
-              console.log("New content loaded successfully!");
-          }
-          if(statusTxt == "error"){
-              console.log("Error: " + jqXHR.status + " " + jqXHR.statusText);
-          }
-      });
+function loadPdf(id) {
+  $("#pdf").load(`/src/material.html #${id}`, function (
+    responseTxt,
+    statusTxt,
+    jqXHR
+  ) {
+    if (statusTxt == "success") {
+      console.log("New content loaded successfully!");
+    }
+    if (statusTxt == "error") {
+      console.log("Error: " + jqXHR.status + " " + jqXHR.statusText);
+    }
   });
-});
+}
